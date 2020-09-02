932d55b7933094150f3c3ad65e16c87fd97aa9d2
JoaoGuilhermeEtec <70458505+JoaoGuilhermeEtec@users.noreply.github.com>
2020-09-01 22:19:42 -0300

    Multiplacador
    Att

--- src/Multiplicador.java
+++ src/Multiplicador.java
@@ -9,12 +9,12 @@ public class Multiplicador {
         double numero1, numero2, total=0;
         String valores="";
 
-        valores = JOptionPane.showInputDialog(null, "first number: ");
+        valores = JOptionPane.showInputDialog(null, "primeiro número: ");
         numero1= Double.parseDouble(valores);
-        valores = JOptionPane.showInputDialog(null, "second number: ");
+        valores = JOptionPane.showInputDialog(null, "segundo número: ");
         numero2 = Double.parseDouble(valores);
         //multiplicação dos valores abaixo
         total = numero1 * numero2;
-        JOptionPane.showMessageDialog(null, "product is: " + total);
+        JOptionPane.showMessageDialog(null, "produto é: " + total);
     }
 }
